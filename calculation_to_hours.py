82d4270d13172d05468dbd73168fc1ee8c51f050
dinspi3 <47669142+dinspi3@users.noreply.github.com>
2021-04-09 20:46:30 +0300

    Add files via upload

--- calculation_to_hours.py
+++ calculation_to_hours.py
@@ -1,9 +1,18 @@
-calculation_to_hours=1000
+calculation_to_hours=50
 name_of_unit="hours"
 
-def days_to_units(num_of_days,custom_message):
-    print(f"{num_of_days}days are {num_of_days*calculation_to_hours} {name_of_unit}")
-    print(custom_message)
+def days_to_units(num_of_days):
+ if num_of_days > 0:
+    return (f"{num_of_days} days are {num_of_days*calculation_to_hours} {name_of_unit}")
+   # print(custom_message)
 
-days_to_units(10,"Awesome !")
+user_input = input("Enter numbers of day : ")
+user_input_number = int(user_input)
+
+calculated_value = days_to_units(user_input_number)
+print(calculated_value)
+
+#my_var = days_to_units(18.3987772,"Awesome !")
+
+#10print (my_var)
 
