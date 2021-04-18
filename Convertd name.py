e9aad56b4b5ac1e4ed3362ea3b9a925190687c0e
dinspi3 <47669142+dinspi3@users.noreply.github.com>
2021-04-18 18:00:13 +0300

    Add files via upload

--- Convertd name.py
+++ Convertd name.py
@@ -1,11 +1,10 @@
 
-lname = input ("Enter you name :")
-fname = input ("Enter you name :")
+lname = input ("Enter you Last name :")
+fname = input ("Enter you First name :")
 stringlname= len(lname)
 stringfname= len(fname)
 slicedString= lname [stringlname::-1]
 slicedString1= fname [stringfname::-1]
-print ('You first and last name is convrted to : ')
+print ('You first and last name is converted to : ')
 
 print (slicedString,slicedString1)
-
